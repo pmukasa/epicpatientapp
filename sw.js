9c856652c37e5f2915ca70251d5b630a8277d94f
Peter Mukasa <100413563+pmukasa@users.noreply.github.com>
2024-04-24 22:30:36 +0200

    fix working on epic fhir only with google api

---
+++ sw.js
@@ -0,0 +1,67 @@
+// The version of the cache.
+const VERSION = "v1";
+
+// The name of the cache
+const CACHE_NAME = `health-tracker-${VERSION}`;
+
+// The static resources that the app needs to function.
+const APP_STATIC_RESOURCES = [
+  "/Documents/epicpatientapp-end_to_end/",
+  "/epicpatientapp-end_to_end/index.html",
+  "/epicpatientapp-end_to_end/launch.html",
+  "/epicpatientapp-end_to_end/app.html",
+  "/epicpatientapp-end_to_end/dist/fhirclient/build/fhir-client.js",
+  "/epicpatientapp-end_to_end/icons/wheel.svg",
+  // "/icons/circle.svg",
+];
+
+// On install, cache the static resources
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    (async () => {
+      const cache = await caches.open(CACHE_NAME);
+      cache.addAll(APP_STATIC_RESOURCES);
+    })(),
+  );
+});
+
+// delete old caches on activate
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    (async () => {
+      const names = await caches.keys();
+      await Promise.all(
+        names.map((name) => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+        }),
+      );
+      await clients.claim();
+    })(),
+  );
+});
+
+// On fetch, intercept server requests
+// and respond with cached responses instead of going to network
+self.addEventListener("fetch", (event) => {
+  // As a single page app, direct app to always go to cached home page.
+  if (event.request.mode === "navigate") {
+    event.respondWith(caches.match("/"));
+    return;
+  }
+
+  // For all other requests, go to the cache first, and then the network.
+  event.respondWith(
+    (async () => {
+      const cache = await caches.open(CACHE_NAME);
+      const cachedResponse = await cache.match(event.request.url);
+      if (cachedResponse) {
+        // Return the cached response if it's available.
+        return cachedResponse;
+      }
+      // If resource isn't in the cache, return a 404.
+      return new Response(null, { status: 404 });
+    })(),
+  );
+});
